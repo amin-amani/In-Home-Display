cd27a165f0325b76d43187018f0c5243e8e28e61
A.Amani <didi1364@gmail.com>
2018-10-27 00:21:42 +0330

    add server code

--- InHomeServer/InHomeServer/Form1.Designer.cs
+++ InHomeServer/InHomeServer/Form1.Designer.cs
@@ -28,18 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.CmbDeviceName = new System.Windows.Forms.ComboBox();
+            this.PgrProgress = new System.Windows.Forms.ProgressBar();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // BtnSearch
+            // 
+            this.BtnSearch.Location = new System.Drawing.Point(81, 12);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(75, 23);
+            this.BtnSearch.TabIndex = 0;
+            this.BtnSearch.Text = "Search";
+            this.BtnSearch.UseVisualStyleBackColor = true;
+            this.BtnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CmbDeviceName
+            // 
+            this.CmbDeviceName.FormattingEnabled = true;
+            this.CmbDeviceName.Location = new System.Drawing.Point(54, 41);
+            this.CmbDeviceName.Name = "CmbDeviceName";
+            this.CmbDeviceName.Size = new System.Drawing.Size(121, 21);
+            this.CmbDeviceName.TabIndex = 1;
+            // 
+            // PgrProgress
+            // 
+            this.PgrProgress.Location = new System.Drawing.Point(12, 68);
+            this.PgrProgress.Name = "PgrProgress";
+            this.PgrProgress.Size = new System.Drawing.Size(260, 23);
+            this.PgrProgress.TabIndex = 2;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(83, 46);
+            this.button1.Location = new System.Drawing.Point(54, 157);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -47,6 +74,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.PgrProgress);
+            this.Controls.Add(this.CmbDeviceName);
+            this.Controls.Add(this.BtnSearch);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,6 +85,9 @@
 
         #endregion
 
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Windows.Forms.ComboBox CmbDeviceName;
+        private System.Windows.Forms.ProgressBar PgrProgress;
         private System.Windows.Forms.Button button1;
     }
 }
--- InHomeServer/InHomeServer/Form1.cs
+++ InHomeServer/InHomeServer/Form1.cs
@@ -9,16 +9,35 @@ using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace InHomeServer
 {
     public partial class Form1 : Form
     {
-        bool SearchCompleted = false;
+        
+        List<string> AvaliableDevices = new List<string>();
+        List<ManualResetEvent> events = new List<ManualResetEvent>();
+        public static event EventHandler OperationCompleted;
+        int MaxDevices = 1;
+        string Host;
+
 
         public Form1()
         {
             InitializeComponent();
+            OperationCompleted += Form1_OperationCompleted;
+        }
+
+        void Form1_OperationCompleted(object sender, EventArgs e)
+        {
+            
+            PgrProgress.Invoke(new MethodInvoker(delegate
+                    {
+                        PgrProgress.Value++;
+                        PgrProgress.Refresh();
+                    }));
+      
         }
         public static string GetLocalIPAddress()
         {
@@ -32,116 +51,107 @@ namespace InHomeServer
             }
             throw new Exception("No network adapters with an IPv4 address in the system!");
         }
-        public void DisplayLocalHostName()
+
+         bool Check(string ip,int port,int timeout)
         {
             try
             {
-                // Get the local computer host name.
-                String hostName = Dns.GetHostName();
-                Console.WriteLine("Computer name :" + hostName);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException caught!!!");
-                Console.WriteLine("Source : " + e.Source);
-                Console.WriteLine("Message : " + e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception caught!!!");
-                Console.WriteLine("Source : " + e.Source);
-                Console.WriteLine("Message : " + e.Message);
-            }
-        }
-        bool Check(string ip,string host,int port,int timeout)
-        {
-            try
-            {
+                if (AvaliableDevices.Count >= MaxDevices) return false;
                 var client = new TcpClient();
                 var result = client.BeginConnect(ip, port, null, null);
-
+                Console.WriteLine("check"+ip);
                 var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(timeout));
 
                 if (!success)
                 {
+                    Console.WriteLine("end......." + ip + " fail");
                     return false;
                 }
 
                 // we have connected
                 client.EndConnect(result);
+                Console.WriteLine("end......."+ip+" succsess");
                 return true;
             }
             catch (Exception ex)
             {
                 return false;
             }
-            //using (TcpClient tcpClient = new TcpClient())
-            //{
 
-            //    try
-            //    {
-             
-            //        tcpClient.Connect(ip, port);
-            //        return true;
-            //       // Console.WriteLine("Port open");
-            //    }
-            //    catch (Exception)
-            //    {
-            //     //   Console.WriteLine("Port closed");
-            //        return false;
-            //    }
-            //}
         
         }
-        void GetAvailableDevices(int start, int end, int port, int timeout,ref List<string> avaliableDevices)
+
+        private void   TaskCallBack(Object ThreadNumber)
         {
-         //  List<string> devices=new List<string>();
+            if (Check(Host + ((int)(ThreadNumber)).ToString(), 1217, 1))
+            {
+                AvaliableDevices.Add (Host + ((int)(ThreadNumber)).ToString());
+                       CmbDeviceName.Invoke(new MethodInvoker(delegate
+                    {
+                        CmbDeviceName.Items.Add(Host + ((int)(ThreadNumber)).ToString());    
+                    }));
+        
+                
+            }
+            OnOperationCompleted();
+            events[(int)(ThreadNumber)-1].Set();
 
-           string IPAddress = GetLocalIPAddress();
-           string[] sp = IPAddress.Split('.');
-           string host = sp[0] + "." + sp[1] + "." + sp[2]+".";
-           for (int i = start; i < end; i++)
-           {
-               string tempIP = host + i.ToString();
-               Console.WriteLine(tempIP);
-               if (Check(tempIP, host, port, timeout))
-               {
-                   avaliableDevices.Add(tempIP);
-                  
-               }
-       
-           }
+        }
 
-          
+        static void OnOperationCompleted()
+        {
+            if (OperationCompleted != null)
+                OperationCompleted(null, EventArgs.Empty);
+        }
 
-          //  return devices;
+        void StartSearch() {
+        
+            events.Clear();
+            AvaliableDevices.Clear();
+            string IPAddress = GetLocalIPAddress();
+            string[] sp = IPAddress.Split('.');
+            Host = sp[0] + "." + sp[1] + "." + sp[2] + ".";
+
+            for (int i = 0; i < 255; i++)
+            {
+                var resetEvent = new ManualResetEvent(false);
+
+                events.Add(resetEvent);
+            }
+
+            Parallel.For(0, 255, new ParallelOptions { MaxDegreeOfParallelism = 20 },
+            task =>
+            {
+                try
+                {
+                    //doneEvents[task] = new ManualResetEvent(false);
+                    //var resetEvent = new ManualResetEvent(false);
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(TaskCallBack), task + 1);
+                }
+                finally
+                {
+
+                }
+                //events.Add(resetEvent);
+
+            });
+
+            foreach (var ev in events)
+                ev.WaitOne();
+
+
+            Console.WriteLine("fin.....................");
+          //  CmbDeviceName.Items.AddRange(AvaliableDevices.ToArray());
+            MessageBox.Show("finished");
+            return;
         
         }
         private void button1_Click(object sender, EventArgs e)
         {
-    
-            Thread[] myNewThread=new Thread[18]; 
-            List<string> avaliableDevices = new List<string>();
-            for (int i = 0; i < 17; i++)
-            {
-                Console.WriteLine((i * 15 + 1).ToString() + "to " + (i * 15 + 15).ToString());
-          
-                 myNewThread[i] = new Thread(() => GetAvailableDevices(i * 15 + 1, i * 15 +15, 1217, 1, ref avaliableDevices));
-                myNewThread[i].Start();
-
-            }
-            for (int i = 1; i < 17; i++)
-            {
-       
-                myNewThread[i].Join();
-            }
-          
-
-
-
-           Console.WriteLine("def");
-
- 
+            PgrProgress.Value = 0;
+            PgrProgress.Maximum = 255;
+    Thread thrSearch = new Thread(new ThreadStart(StartSearch));
+    thrSearch.Start();
         
         }
     }
